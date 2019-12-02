61b66164a398068a82db23af963a9e8f00f9657c
Rudolfs Agris Stilve <rudolfsagris@gmail.com>
2019-12-02 13:12:16 +0200

    Added important feature

--- src/main/java/RTU_example/Main.java
+++ src/main/java/RTU_example/Main.java
@@ -3,5 +3,6 @@ package RTU_example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+		Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "RTU_example"});
     }
 }
